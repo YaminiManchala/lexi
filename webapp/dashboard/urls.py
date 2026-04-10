4ab212b1ad7f5a9b29658246ca5c99ce9aa86b31
MinMin <manchalasrinu05@gmail.com>
2026-04-10 07:38:27 +0530

    code

--- webapp/dashboard/urls.py
+++ webapp/dashboard/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('scan/', views.output_view, name='scan'),
+    path('scan/', views.output_view, name='scan'), 
+    path('download/', views.download_report, name='download'),
 ]
